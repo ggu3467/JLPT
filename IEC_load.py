600580e970c0c968cbcbf069efdb287d4a54948d
GGU3467 <ggu3467@gmail.com>
2023-01-24 19:53:47 +0100

    Début lecture Excel

--- IEC_load.py
+++ IEC_load.py
@@ -28,74 +28,87 @@ import xlrd as XLRD
 # @return sclMgr    : Point d'accès aux services de la librairie 'scl_loader'
 # @return data      : Image de la section DataTypeTemplates du SCL.
 # @return tComm     : Image de la section Communication du SCL
-class Load_Excel(object):
+
+class Question:
+    def __init__(self, question_text, correct_answer, hint='' ):
+        self.question = question_text
+        self.correct_answer = correct_answer
+        self.hint   = hint
+class Proposition:
+    def __init__(self, reponse1, reponse2, reponse3, reponse4, reponse5='', reponse6='', answer='', hint='' ):
+        self.reponse1 = reponse1
+        self.reponse2 = reponse2
+        self.reponse3 = reponse3
+        self.reponse4 = reponse4
+        self.reponse5 = reponse5
+        self.reponse6 = reponse6
+        self.answer  = answer
+        self.hint    = hint
+
+class LoadExcel(object):
     def __init__(self, _name: str, _load_excel:bool = False, _fullpath=False):
         self.file_name  = _name
-        self.fullPath   = _fullpath
+        self.full_path   = _fullpath
         self.load_excel = _load_excel
 
     def __enter__(self):
+        test_set = XLRD.open_workbook(self.file_name)
+        sheet = test_set.sheet_by_index(0) 
+        line_index = 1
 
-#        Test_Set = XLRD.open_workbook(self.file_name)
-        Test_Set = XLRD.open_workbook(self.file_name)
-        sheet = Test_Set.sheet_by_index(0) 
-        LineIndex = 1
-        
         # for extracting multiple rows at a time
-        for i in range(LineIndex, sheet.nrows): 
-            Title = sheet.cell_value(i, 0)
+        for i in range(line_index, sheet.nrows):
+            title = sheet.cell_value(i, 0)
 
 # Détection d'un grand chapître
-            if Title.startswith('問題'):
-                print("Chapitre de question:" + '問題' + ':' + Title[2])
-                # TODO récupérer le numéro de chapître
-                print('Chapitre de question' + Title)
-                LineIndex = LineIndex +1
+            if title.startswith('問題'):
+                print("Chapitre de question:" + '問題' + ':' + title[2])
+# TODO récupérer le numéro de chapître
+                print('Chapitre de question' + title)
+                line_index = line_index +1
 
-                while (True):
-                    Topic = sheet.cell_value(LineIndex, 1)
-                    LineIndex = LineIndex +1
+                while True:
+                    topic = sheet.cell_value(line_index, 1)
+                    line_index = line_index +1
 # Détection d'une phrase qui fera l'objet de questio
-                    if Topic.startswith('問'):                    
-                        Question = Topic
-                        Number = Topic[1]
-                        print("Topic:" + Topic + ':' + str(Number))
-                        LineIndex = self.GetQuestionList(sheet, LineIndex)
+                    if topic.startswith('問'):
+                        number = topic[1]
+                        print("Topic:" + topic + ':' + str(number))
+                        line_index = self.GetQuestionList(sheet, line_index)
             
             else:
                 print('xxx')
 
-    def GetQuestionList(self, sheet, LineIndex):
+    def GetQuestionList(self, sheet, line_index:int):
                         
-        QuestionList=[]
-        GetQuestion = True
-        while GetQuestion is True:
-            
-            Question = sheet.cell_value(LineIndex, 2)
-            NumQuestion = []
-            if Question.startswith('(') or Question.startswith('（'):
-                NumQuestion.append('Q: ' + Question)
 
-                ListeChoix = Question.split('．')
-                NumQuestion  = ListeChoix[0]
-                Topic        = ListeChoix[1].split(' ')
-                print('Choix' + NumQuestion + Topic[1] + Topic[0])
-                Proposition = []
+        get_question = True
+        while get_question is True:
+            
+            question = sheet.cell_value(line_index, 2)
+            num_question = []
+            if question.startswith('(') or question.startswith('（'):
+                num_question.append('Q: ' + question)
+
+                liste_choix = question.split('．')
+                num_question  = liste_choix[0]
+                topic        = liste_choix[1].split(' ')
+                print('Choix' + num_question + topic[1] + topic[0])
+                proposition = []
                 for i in range(1,5):
-                    Proposition.append(ListeChoix[i])
+                    proposition.append(liste_choix[i])
 
                 for i in range(0,4):
-                    print('Choix:' +  Proposition[i] + '\n')
+                    print('Choix:' +  proposition[i] + '\n')
 
             else:
                 break
 
-            LineIndex = LineIndex +1
+            line_index = line_index +1
     
-        for Question in QuestionList:
-            print('Question:'  + Question)
 
-        return LineIndex
+
+        return line_index
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         logging.info("Chargement fichier JLPT:" + str(self.file_name[0]) + "réussi.")
@@ -104,7 +117,7 @@ class Load_Excel(object):
 
 if __name__ == '__main__':
 
-    with Load_Excel("JLPT_3_ESSAI_2003.xls", True, False) as (JLPT_TestSet):  # , self.T_LoadSCL):
+    with LoadExcel("JLPT_3_ESSAI_2003.xls", True, False) as (JLPT_TestSet):  # , self.T_LoadSCL):
         print("Chargement SCL ok")  # str(self.T_LoadSCL))
 
 
