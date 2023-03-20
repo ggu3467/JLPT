136f1161e1b0056e5527d0eccca343b94f204b14
ggu3467 <gguillet@gmail.com>
2023-03-20 17:59:49 +0100

    Modifications...

--- IEC_load.py
+++ IEC_load.py
@@ -56,74 +56,85 @@ class Proposition:
 class Test_JLPT:
     def __init__(self, sheet):
         self.sheet = sheet
-        self.line_index = 1
         self.TEST = JLPT(1991, 'Level4')
         self.Chapitre = [ '問題Ⅰ' , '問題ⅠⅠ', '問題Ⅲ' ]
-        self.section  = [ 1 , 2 , 3]
+        self.Section  = [ 1 , 2 , 3]
         self.Consigne = ""         # " 問題Ⅰ＿＿＿のことばはどうよみますか"
         self.Part1 = [] # QuestionX()
         self.Part2 = [] #QuestionX()
+        self.lineIndex = 1
 
+        self.TestSection     = 0        # Colonne 0 Titre 問題Ⅰ＿＿＿のことばはどうよみますか
+        self.TestQuestion    = 1        # Colonne 1     問1・ 車の中に男の子が何人いますか。
+        self.TestProposition = 2        # Colonne 2     （1）．車 １．しゃ ２．くるま ３．ちゃ ４．くろま
 
     def TestPart(self):
         num_titre = 1
-        for Chapitre, Numero in zip (self.Chapitre,self.section):
-            if Numero == 1:
-                Titre = self.sheet.cell_value(self.line_index, 0)
-                Chapitre_1 = JLPT(1991,'Level 4')
-                self.Part1 = self.ParseTest(self.line_index+1)
-                num_titre = num_titre + 1
+        self.lineIndex  = 1
+        T1 = self.sheet.cell_value(self.lineIndex ,   self.TestSection)       # Titre 問題Ⅰ＿＿＿のことばはどうよみますか
+        if not T1.startswith('問題'):
+            print('Manque 問題 sur première ligne' )
+
+        Q1 = self.sheet.cell_value(self.lineIndex+1 , self.TestQuestion)      # 問1・ 車の中に男の子が何人いますか。
+        if not Q1.startswith('問'):
+            print('Manque 問 sur deuxième ligne' )
+
+        P1 = self.sheet.cell_value(self.lineIndex+2 , self.TestProposition)   #（1）．車 １．しゃ ２．くるま ３．ちゃ ４．くろま
+        if not P1.startswith('（1）'):
+            print('Manque （1） sur troisième ligne' )
+
+ 
+        T1 = self.sheet.cell_value(self.lineIndex , 0)
+
+        for Chapitre, Numero in zip (self.Chapitre,self.Section):
+            Chapitre_1 = JLPT(1991,'Level 4')
+            self.Part = self.ParseTest()
+
+#            self.Part2,line_index = self.ParseTest(line_index)
+#            self.Part3,line_index = self.ParseTest(line_index)
+            num_titre = num_titre + 1
         return self.TEST
 
-    def ParseTest(self, line_index):
-
-        _question = self.sheet.cell_value(line_index, 1)  # Exemple:
+    def ParseTest(self):
+        self.lineIndex = 2
+        _question = self.sheet.cell_value(self.lineIndex , self.TestQuestion)  # Exemple:
         question = []  #Liste des questions
         while _question.startswith('問'):      # Détection d'une phrase qui fera l'objet de question
-            
+            print('Question:' + _question)
             question = Question(_question,[])
-          
-            line_index = line_index+1
+            self.lineIndex =  self.lineIndex + 1
+
             while(True):
-                try:
-                    Choix,line_index = self.GetProposition(self.sheet, line_index)
-                    if Choix is not None:
-                        question.answer.append(Choix)
-                    else:
-                        QuestionText = self.sheet.cell_value(line_index, 1)
+                Choix,line_index = self.GetProposition(self.sheet)
+                if Choix is not None:
+                    question.answer.append(Choix)
+                else:                           
+                    QuestionText = self.sheet.cell_value( self.lineIndex, 1)
+                    if QuestionText.startswith('問'):
                         question.subQuestion.append(QuestionText)
-                except Exception as e:
-                    print(e)
-                    return question
-                line_index = line_index + 1
+                        self.lineIndex = self.lineIndex + 1
+                        break
+                self.lineIndex = self.lineIndex + 1
             
             print('-------------')
         return question
 
 
 
-    def GetProposition(self, sheet, line_index:int):
-        try:
-            TxtQuestion =  sheet.cell_value(line_index, 2)
-            print("Sub Question" + TxtQuestion)
-        except IndexError:
-            print('fin du fichier..')
-            return None, line_index
+    def GetProposition(self, sheet):
+        TxtQuestion =  sheet.cell_value(self.lineIndex, 2)
+        print("Sub Question" + TxtQuestion)
 
         proposition = Proposition(TxtQuestion)
         if TxtQuestion.startswith('(') or TxtQuestion.startswith('（'):    # ASCII et UTF8 pour '('
             liste_choix = TxtQuestion.split('．')
             num_question  = liste_choix[0]
             topic        = liste_choix[1].split(' ')
-
-#            proposition.hint   = ''
-#            proposition.answer = ''
             for i in range(1,len(liste_choix)):
                 proposition.reponse.append(liste_choix[i])
 
-            return proposition, line_index
-        else:
-            return None, line_index
+            return proposition, self.lineIndex
+        return None, self.lineIndex
 
 ## \b LoadSCL:  classe générique pour charger un fichier SCL au niveau fichier et au niveau.
 # Le code permet de s'abstraire des problèmes liés à des OS spécifiques
